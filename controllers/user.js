75f6d7b18f9c6d06e23c9d7476c0765d7b3b8977
Hariake <tosarv@gmail.com>
2024-09-11 13:23:14 +0300

    Create user login

--- controllers/user.js
+++ controllers/user.js
@@ -40,6 +40,28 @@ class userController{
                 })
             } 
     } 
+    async login(req, res){
+        const {username, password} = req.body;
+        
+
+        const user = await UserModel.findOne(username);
+        if (!user){
+            return res.status(400).json({ error: 'username doesnt exist'}) 
+        } 
+
+        const isPassValid = await bcrypt.compare(password, user.password);
+        if (!isPassValid){
+            return res.status(400).json({ error: 'wrong password'})
+        } 
+        req.session.user = {
+            username: user.username,
+            user_id: user.id
+        } 
+        res.json({
+            message: 'Login succsessful',
+            user_session: req.session.user
+        })
+    } 
 } 
 
 module.exports =  new userController()
